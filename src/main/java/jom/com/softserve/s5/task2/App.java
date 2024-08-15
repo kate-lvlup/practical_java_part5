c31a3401d4b1bf2e63330951163c49b2a1a8ea64
kate-lvlup <kate.yalanskaya@gmail.com>
2024-08-15 11:30:17 +0300

    add task2 static Consumer<double[]> cons

--- src/main/java/jom/com/softserve/s5/task2/App.java
+++ src/main/java/jom/com/softserve/s5/task2/App.java
@@ -1,10 +1,22 @@
 package jom.com.softserve.s5.task2;
 
+import java.util.Arrays;
+import java.util.function.Consumer;
+
 public class App {
-    public static double[] getChanged(
-            double[] initialArray
-            /*place the second parameter here*/    ) {
-    	//your code
-    	return null;
+    public static Consumer<double[]> cons = (double[] array) -> {
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] > 2) {
+                array[i] = array[i] * 0.8;
+            } else {
+                array[i] = array[i] * 0.9;
+            }
+        }
+    };
+
+    public static double[] getChanged(double[] initialArray, Consumer<double[]> consumer) {
+        double[] newArray = Arrays.copyOf(initialArray, initialArray.length);
+        consumer.accept(newArray);
+        return newArray;
     }
 }
