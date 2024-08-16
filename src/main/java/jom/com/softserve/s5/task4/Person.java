7c0dd4cc5a122f69c125c09bede787ff9c37de5b
kate-lvlup <kate.yalanskaya@gmail.com>
2024-08-16 11:33:31 +0300

    add task4 method: how many people in a group will go shopping if they are offered certain discount on product

--- src/main/java/jom/com/softserve/s5/task4/Person.java
+++ src/main/java/jom/com/softserve/s5/task4/Person.java
@@ -1,23 +1,42 @@
 package jom.com.softserve.s5.task4;
 
+import java.util.ArrayList;
+import java.util.List;
+
 class Person {
-	String name;
+    private String name;
+    DecisionMethod goShopping;
 
-	Person(String name) {
-		this.name = name;
-	}
+    public Person(String name) {
+        this.name = name;
+        this.goShopping = (productName, discount) -> "product1".equals(productName) && discount > 10;
+    }
 
-//	DecisionMethod goShopping // Write lambda here 
+    public String getName() {
+        return name;
+    }
+
+    public void setGoShopping(DecisionMethod goShopping) {
+        this.goShopping = goShopping;
+    }
+
+    public boolean willGoShopping(String productName, int discount) {
+        return goShopping.decide(productName, discount);
+    }
 }
 
-//Create DecisionMethod interface here
+@FunctionalInterface
+interface DecisionMethod {
+    boolean decide(String productName, int discount);
+}
 
 
 class Shop {
-//	public List<DecisionMethod> clients = new ArrayList<>();
+    List<DecisionMethod> clients = new ArrayList<>();
 
-	public int sale(String product, int percent) {
-		// Write your code here
-		return 0;
-	}
+    public int sale(String productName, int discount) {
+        return (int) clients.stream()
+                .filter(decisionMethod -> decisionMethod.decide(productName, discount))
+                .count();
+    }
 }
