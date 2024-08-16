473521e8109852921593d971037fe28f28ce4de8
kate-lvlup <kate.yalanskaya@gmail.com>
2024-08-16 11:45:50 +0300

    add task5 method: getPredicateFromSet(Set<Predicate<Integer>> predicateSet)

--- src/main/java/jom/com/softserve/s5/task5/MyUtils.java
+++ src/main/java/jom/com/softserve/s5/task5/MyUtils.java
@@ -1,5 +1,12 @@
 package jom.com.softserve.s5.task5;
 
+import java.util.Set;
+import java.util.function.Predicate;
+
 class MyUtils {
-    // Write your code here
+    public static Predicate<Integer> getPredicateFromSet(Set<Predicate<Integer>> predicateSet) {
+        return predicateSet.stream()
+                .reduce((integerPredicate, other) -> integerPredicate.and(other))
+                .orElse(x -> true);
+    }
 }
